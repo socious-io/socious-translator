d7afd5360eb6e1d7cced3f66030325849165bdef
Alexis Baraka Mcharo <alexismcharo@Alexiss-MacBook-Air.local>
2025-08-22 15:30:25 +1000

    fixing hallucination pt 5

--- main.py
+++ main.py
@@ -342,15 +342,14 @@ async def websocket_endpoint(websocket: WebSocket):
                     result = model.transcribe(
                         wav.name,
                         fp16=True,
-                        temperature=(0.0, 0.2, 0.4, 0.6),
-                        best_of=5,
-                        beam_size=None,
+                        temperature=0.0
+                        beam_size=5,
                         condition_on_previous_text=False,
                         hallucination_silence_threshold=0.30,
-                        no_speech_threshold=0.85,
+                        no_speech_threshold=0.6,
                         language="en" if source_lang == "English" else "ja",
-                        compression_ratio_threshold=2.0,
-                        logprob_threshold=-1.2,
+                        compression_ratio_threshold=1.8,
+                        logprob_threshold=-0.4,
                         task=whisper_task,
                     )
 
